cc8c6a7efd60e0d0ef43c04eb06f64fc5141ffa1
sayalisankhe5 <86399171+sayalisankhe5@users.noreply.github.com>
2022-07-05 13:27:29 +0530

    Persistance example

---
+++ PersistanceExample/ClientAppp/Program.cs
@@ -0,0 +1,32 @@
+﻿using PersistanceExample;
+using System;
+using static PersistanceExample.Persister;
+using static PersistanceExample.XMLPersister;
+
+namespace ClientAppp
+{
+    [TargetPersistaneType(PersistanceType.Xml)]
+    public class PatientDataModel
+    {
+        //[TransformToRelated(TransformTo.Attribute)]
+        [XmlAttribute]
+        public string MRN { get; set; }
+
+        //[TransformToRelated(TransformTo.Element)]
+        [XmlElement]
+        public string Name { get; set; }
+
+        //[TransformToRelated(TransformTo.Attribute)]
+        [XmlIgnore]
+        public int Age { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PatientDataModel model = new PatientDataModel() { MRN = "M100", Name = "Tom", Age = 33 };
+            Persister _persister = new Persister();
+            _persister.Persist(model);
+        }
+    }
+}
---
+++ PersistanceExample/PersistanceExample/Persister.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Linq;
+using static PersistanceExample.Persister;
+
+namespace PersistanceExample
+{
+    public class Persister
+    {
+        public enum PersistanceType
+        {
+            Xml,Json,Binary,Csv
+        }
+
+        [AttributeUsage(AttributeTargets.Class,AllowMultiple = false)]
+        public class TargetPersistaneTypeAttribute : Attribute
+        {
+            public PersistanceType format;
+            public TargetPersistaneTypeAttribute(PersistanceType format)
+            {
+                this.format = format;
+            }
+        }
+
+        public bool Persist(object source)
+        {
+            var targetTypeAttribute = source.GetType().GetCustomAttributes(typeof(TargetPersistaneTypeAttribute), true).FirstOrDefault() as TargetPersistaneTypeAttribute;
+            PersistanceType _targetFormat = targetTypeAttribute.format;
+            Console.WriteLine($"Target format is : {_targetFormat} ");
+            switch (_targetFormat)
+            {
+                case PersistanceType.Xml:
+                    XMLPersister _persister = new XMLPersister();
+                    _persister.WriteObject(source);
+                    break;
+            }
+            return false;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property,AllowMultiple = false)]
+    public class XmlPersisterAttribute:Attribute
+    {
+
+    }
+
+    public class XmlAttributeAttribute : XmlPersisterAttribute
+    {
+
+    }
+    public class XmlElementAttribute : XmlPersisterAttribute
+    {
+
+    }
+    public class XmlIgnoreAttribute : XmlPersisterAttribute
+    {
+
+    }
+
+    public class XMLPersister
+    {
+        //public enum TransformTo
+        //{
+        //    Attribute, Element
+        //}
+
+        //[AttributeUsage(AttributeTargets.Property)]
+        //public class TransformToRelatedAttribute : Attribute
+        //{
+        //    public TransformTo transformTo;
+        //    public TransformToRelatedAttribute(TransformTo format)
+        //    {
+        //        this.transformTo = format;
+        //    }
+        //}
+        public void WriteObject(object source)
+        {
+            var allProperties = source.GetType().GetProperties();
+            //for(int i=0;i<allProperties.Length;i++)
+            //{
+            //    var changeTo = allProperties[i].GetCustomAttributes(typeof(TransformToRelatedAttribute), true).FirstOrDefault() as TransformToRelatedAttribute;
+            //    TransformTo _transformTo = changeTo.transformTo;
+            //    Console.WriteLine($"Property  : {(allProperties[i]).Name} and change to {_transformTo}");
+            //}
+
+            for (int i = 0; i < allProperties.Length; i++)
+            {
+                var customAttributes = allProperties[i].GetCustomAttributes(typeof(XmlPersisterAttribute), true) as XmlPersisterAttribute[];
+                
+                foreach(var xmlAttribute in customAttributes)
+                {
+                    if(xmlAttribute is XmlAttributeAttribute)
+                    {
+                        Console.WriteLine($"Property  : {(allProperties[i]).Name} and change to {xmlAttribute}");
+                    }
+                    if(xmlAttribute is XmlElementAttribute)
+                    {
+                        Console.WriteLine($"Property  : {(allProperties[i]).Name} and change to {xmlAttribute}");
+                    }
+                    if(xmlAttribute is XmlIgnoreAttribute)
+                    {
+                        Console.WriteLine($"Property  : {(allProperties[i]).Name} and change to {xmlAttribute}");
+                    }
+
+                }
+            }
+
+
+        }
+    }
+}
